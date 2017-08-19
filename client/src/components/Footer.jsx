178e25aaee1981599cbcee35ed8bb419cdf534a5
Daniel W Beebe <danwbeebe@gmail.com>
2017-08-19 17:08:30 -0400

    Daniel - Create components (#14)
    * edits to proposal file
    
    * add user story to proposal
    
    * edit readme and proposal
    
    * edits to readme file
    
    * move photo of main menu screen in readme file
    
    * create components
    
    * add code for Header and Footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render(){
+    return (
+      <div className='footer'>
+        <h2>Created by Brandon, Briseida, Dan & Ivan</h2>
+      </div>
+    )
+  }
+}
+
+export default Header;
