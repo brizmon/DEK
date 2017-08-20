a0a68dcd7e72c09d991f414094b5fe8580e5f570
Daniel W Beebe <danwbeebe@gmail.com>
2017-08-20 17:01:01 -0400

    Daniel - Add router npm package and app.js edits for router (#16)
    * edits to proposal file
    
    * add user story to proposal
    
    * edit readme and proposal
    
    * edits to readme file
    
    * move photo of main menu screen in readme file
    
    * create components
    
    * add code for Header and Footer
    
    * add react router dom package and edit app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+import Home from './components/Welcome';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <main>
-        </main>
-        <Footer />
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <div className="main">
+            <Route exact path="/" component={Welcome} />
+          </div>
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
