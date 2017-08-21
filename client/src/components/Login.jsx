e2dd662f836acc31a6f2ee9bf8a1fcdf28978c84
brizmon <brizism@gmail.com>
2017-08-21 18:26:02 -0400

    Login page css (#41)
    * add div to wrap login form
    
    * add styles to login page

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -16,13 +16,15 @@ class Login extends Component{
 
     render(){
         return(
-            <form action="/auth/login" method='POST'>
-                <input type="text" name='userName' placeholder='User Name'
-                onChange={(e)=>this.handleInputChange(e)}/>
-                <input type="password" name='password' placeholder='Password'
-                onChange={(e)=>this.handleInputChange(e)}/>
-                <input onClick={(e)=>this.props.handleLoginSubmit(e,this.state.userName, this.state.password)} type="submit" value='Submit'/>
-            </form>   
+            <div className="login-container">
+                <form action="/auth/login" method='POST'>
+                    <input type="text" name='userName' placeholder='User Name'
+                    onChange={(e)=>this.handleInputChange(e)}/>
+                    <input type="password" name='password' placeholder='Password'
+                    onChange={(e)=>this.handleInputChange(e)}/>
+                    <input onClick={(e)=>this.props.handleLoginSubmit(e,this.state.userName, this.state.password)} type="submit" value='Submit'/>
+                </form>  
+            </div> 
         )
     }
 }
