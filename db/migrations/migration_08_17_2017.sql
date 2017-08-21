648395a9f04460e82d70c08c04abf6d074cbf1fa
BStudies <bhew@eg.poly.edu>
2017-08-21 16:08:50 -0400

    Auth register is now live (#36)

--- db/migrations/migration_08_17_2017.sql
+++ db/migrations/migration_08_17_2017.sql
@@ -9,20 +9,20 @@ CREATE TABLE users(
     lastname VARCHAR
 );
 
-DROP TABLE IF EXISTS routinedek;
-CREATE TABLE routinedek(
-    id BIGSERIAL PRIMARY KEY,
-    question VARCHAR (255),
-    answer VARCHAR (255),
-    user_id REFERENCES users(id)
-)
+-- DROP TABLE IF EXISTS routinedek;
+-- CREATE TABLE routinedek(
+--     id BIGSERIAL PRIMARY KEY,
+--     question VARCHAR (255),
+--     answer VARCHAR (255),
+--     user_id REFERENCES users(id)
+-- )
 
-DROP TABLE IF EXISTS spacedek;
-CREATE TABLE spacedek(
-    id BIGSERIAL PRIMARY KEY,
-    question VARCHAR (255),
-    answer VARCHAR (255),
-    routinedek REFERENCES routinedek(id)
-)
+-- DROP TABLE IF EXISTS spacedek;
+-- CREATE TABLE spacedek(
+--     id BIGSERIAL PRIMARY KEY,
+--     question VARCHAR (255),
+--     answer VARCHAR (255),
+--     routinedek REFERENCES routinedek(id)
+-- )
 
 
