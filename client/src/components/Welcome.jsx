8f6150cb3314e1db418adcb767e764cc137008a3
brizmon <brizism@gmail.com>
2017-08-22 09:09:22 -0400

    Welcome page styles (#44)
    * change styles to welcome page and add axis webfont
    
    * create img folder inside public and add border.svg
    
    * add last styles to welcome page

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -5,14 +5,19 @@ const Welcome = () => {
 
 
   return (
-    <div className='welcome'>
-      <h1>Dek</h1>
-      <h2>Short intro: what DEK is about...</h2>
-      <div className='auth-buttons'>
-        <Link to="/register"><button>Sign Up</button></Link>
-        <Link to="/login"><button>Log In</button></Link>
+      <div className="white-border">
+        <div className="card-tag"></div>
+        <div className="card-tag-two"></div>
+        <div className="border"></div>
+        <div className='welcome'>
+          <h1>Dek</h1>
+          <h2>Short intro: what DEK is about...</h2>
+          <div className='auth-buttons'>
+            <Link to="/register"><button>Sign Up</button></Link>
+            <Link to="/login"><button>Log In</button></Link>
+          </div>
+        </div>
       </div>
-    </div>
     )
 }
 
