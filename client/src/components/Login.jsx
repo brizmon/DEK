93847bf1662b66c8d9ea940ad8b81c4e60c60e25
brizmon <brizism@gmail.com>
2017-08-22 09:11:47 -0400

    Login page updated styles (#46)
    * change styles to welcome page and add axis webfont
    
    * create img folder inside public and add border.svg
    
    * add last styles to welcome page
    
    * add last styles to register page
    
    * css updates on login page

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -17,13 +17,17 @@ class Login extends Component{
     render(){
         return(
             <div className="login-container">
-                <form action="/auth/login" method='POST'>
-                    <input type="text" name='userName' placeholder='User Name'
-                    onChange={(e)=>this.handleInputChange(e)}/>
-                    <input type="password" name='password' placeholder='Password'
-                    onChange={(e)=>this.handleInputChange(e)}/>
-                    <input onClick={(e)=>this.props.handleLoginSubmit(e,this.state.userName, this.state.password)} type="submit" value='Submit'/>
-                </form>  
+                <div className="border"></div>
+                <div className="login-wrap">
+                    <h1>Log In</h1>
+                    <form action="/auth/login" method='POST'>
+                        <input type="text" name='userName' placeholder='User Name'
+                        onChange={(e)=>this.handleInputChange(e)}/>
+                        <input type="password" name='password' placeholder='Password'
+                        onChange={(e)=>this.handleInputChange(e)}/>
+                        <input onClick={(e)=>this.props.handleLoginSubmit(e,this.state.userName, this.state.password)} type="submit" value='Submit'/>
+                    </form>  
+                </div>
             </div> 
         )
     }
