717d32e7c13185b9ed451392fd666ddd9906e3e8
brizmon <brizism@gmail.com>
2017-08-22 09:10:42 -0400

    Register page styles (#45)
    * change styles to welcome page and add axis webfont
    
    * create img folder inside public and add border.svg
    
    * add last styles to welcome page
    
    * add last styles to register page

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -17,19 +17,23 @@ class Register extends Component{
     render(){
         return(
             <div className="register-container">
-                <form action="/auth/register" method='POST'>
-                    <input type="text" name='firstName' placeholder='First Name'
-                    onChange={(e)=>this.handleInputChange(e)}/>
-                    <input type="text" name='lastName' placeholder='Last Name'
-                    onChange={(e)=>this.handleInputChange(e)}/>
-                    <input type="text" name='userName' placeholder='User Name'
-                    onChange={(e)=>this.handleInputChange(e)}/>
-                    <input type="email" name='email' placeholder='Email'
-                    onChange={(e)=>this.handleInputChange(e)}/>
-                    <input type="password" name='password' placeholder='Password'
-                    onChange={(e)=>this.handleInputChange(e)}/>
-                    <input onClick={(e)=>this.props.handleRegisterSubmit(e, this.state)} type="submit" value='Submit'/>
-                </form>
+                <div className="border"></div>
+                <div className="register-wrap">
+                    <h1>Sign Up</h1>
+                    <form action="/auth/register" method='POST'>
+                        <input type="text" name='firstName' placeholder='First Name'
+                        onChange={(e)=>this.handleInputChange(e)}/>
+                        <input type="text" name='lastName' placeholder='Last Name'
+                        onChange={(e)=>this.handleInputChange(e)}/>
+                        <input type="text" name='userName' placeholder='User Name'
+                        onChange={(e)=>this.handleInputChange(e)}/>
+                        <input type="email" name='email' placeholder='Email'
+                        onChange={(e)=>this.handleInputChange(e)}/>
+                        <input type="password" name='password' placeholder='Password'
+                        onChange={(e)=>this.handleInputChange(e)}/>
+                        <input onClick={(e)=>this.props.handleRegisterSubmit(e, this.state)} type="submit" value='Submit'/>
+                    </form>
+                </div>
             </div>
         )
     }
