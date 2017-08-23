43976f087073a088c12deda76915440fb55dd308
brizmon <brizism@gmail.com>
2017-08-23 10:45:53 -0400

    add styles to quiz page (#80)

--- client/src/components/PickQuizType.jsx
+++ client/src/components/PickQuizType.jsx
@@ -12,8 +12,8 @@ const PickQuizType = (props) => {
 
       <h2>Pick Your Quiz Type!</h2>
       <div className='pickquiz-form'>
-          <Link className='pickquiz-button' Link to="/quizscreen">Basic Quiz (review all cards)</Link>
-          <Link className='pickquiz-button' Link to="/quizscreen">Curated Quiz (spaced repetition)</Link>
+          <Link className='pickquiz-button' Link to="/quizscreen">Basic Quiz<p>(review all cards)</p></Link>
+          <Link className='pickquiz-button' Link to="/quizscreen">Curated Quiz<p>(spaced repetition)</p></Link>
       </div>
 
       <Footer />
@@ -22,3 +22,5 @@ const PickQuizType = (props) => {
 }
 
 export default PickQuizType;
+
+
