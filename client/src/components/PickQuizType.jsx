7d763ea580a3ac1af823f57f558d26ec0bc7ac86
brizmon <brizism@gmail.com>
2017-08-23 11:20:24 -0400

    General updates (#84)
    * update style to links in quiz page
    
    * add hover to dek logo

--- client/src/components/PickQuizType.jsx
+++ client/src/components/PickQuizType.jsx
@@ -12,8 +12,8 @@ const PickQuizType = (props) => {
 
       <h2>Pick Your Quiz Type!</h2>
       <div className='pickquiz-form'>
-          <Link className='pickquiz-button' Link to="/quizscreen">Basic Quiz<p>(review all cards)</p></Link>
-          <Link className='pickquiz-button' Link to="/quizscreen">Curated Quiz<p>(spaced repetition)</p></Link>
+          <Link className='pickquiz-button' Link to="/quizscreen"><p className="quiz_link">Basic Quiz</p><p>(review all cards)</p></Link>
+          <Link className='pickquiz-button' Link to="/quizscreen"><p className="quiz_link">Curated Quiz</p><p>(spaced repetition)</p></Link>
       </div>
 
       <Footer />
