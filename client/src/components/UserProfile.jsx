174f683b00e4271738bc856c0be3491664372d3d
brizmon <brizism@gmail.com>
2017-08-23 10:04:24 -0400

    add style to user profile page (#76)

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -11,12 +11,12 @@ const UserProfile = (props) => {
       <Header />
 
       <div className="user-profile-info">
-       <h2>User Profile</h2>
-        <h3>User #: {props.id}</h3>
+       <h2>Your Profile</h2>
+        <h3>User ID: {props.id}</h3>
         <h3>Username: {props.username}</h3>
         <h3>First Name: {props.firstname}</h3>
         <h3>Last Name: {props.lastname}</h3>
-        <h3>E-mail address: {props.email}</h3>
+        <h3>E-mail: {props.email}</h3>
       </div>
 
       <Footer />
