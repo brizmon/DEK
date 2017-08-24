5c73a8bff74e8d45f9b00504f92dd06e9bb922e5
Daniel W Beebe <danwbeebe@gmail.com>
2017-08-24 15:14:45 -0400

    css edits (#102)

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -14,8 +14,8 @@ const Welcome = () => {
             <h2>DEK is an app that helps you learn fast by determining what material you know well and then focusing your studies on material you find challenging and material you haven't studied recently.</h2>
           </div>
           <div className='auth-buttons'>
-            <Link to="/register"><button>Sign Up</button></Link>
-            <Link to="/login"><button>Log In</button></Link>
+            <Link to="/register"><button className="auth-button-1">Sign Up</button></Link>
+            <Link to="/login"><button className="auth-button-2">Log In</button></Link>
           </div>
         </div>
       </div>
