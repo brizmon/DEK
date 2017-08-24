884cccea51f1ff2eb001da52684d99ded7292f15
Daniel W Beebe <danwbeebe@gmail.com>
2017-08-24 15:49:03 -0400

    Daniel (#104)
    * CSS style edits
    
    * rounding edges of register and login forms

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -9,14 +9,17 @@ const Welcome = () => {
         <div className="card-tag-two"></div>
         <div className="border"></div>
         <div className='welcome'>
+
           <h1>DEK</h1>
           <div className="welcome-message">
             <h2>DEK is an app that helps you learn fast by determining what material you know well and then focusing your studies on material you find challenging and material you haven't studied recently.</h2>
           </div>
+
           <div className='auth-buttons'>
             <Link to="/register"><button className="auth-button-1">Sign Up</button></Link>
             <Link to="/login"><button className="auth-button-2">Log In</button></Link>
           </div>
+
         </div>
       </div>
     )
