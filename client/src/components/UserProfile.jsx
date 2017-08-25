e018270b4bb97a8085fb5aa0c9a1a93f858b01a6
BStudies <bhew@eg.poly.edu>
2017-08-25 11:50:58 -0400

    B (#126)
    * Auth register is now live
    
    * Updated creation of deck.js
    
    * work on create card
    
    * Verify backend sends full deck or one card
    
    * fix redirecting bug
    
    * working on refreshing form for create card
    
    * Fix No card quiz screen for all quizes
    
    * Add Card number to user profile

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -1,26 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Header from './Header';
 import Footer from './Footer';
+import Card from './Card.jsx'
+import axios from 'axios'
+class UserProfile extends Component{
+  constructor(){
+    super();
+    this.state = {
+      gotCards: false,
+    }
+  }
 
-const UserProfile = (props) => {
 
-  return (
-    <div className='user-profile'>
-      <Header />
+  getCards = () => {
+    let cards = [];
+    axios.get('/decks', {
+      user_id: this.props.id
+    })
+    .then(res => {
+      console.log(res.data);
+      // put all cards into array of tags
+      for(let j = 0; j < res.data.length; ++j){
+        cards.push(
+          <Card card={res.data[j]}/>
+        )
+      }
+      this.setState({
+        cards: cards,
+        gotCards: true,
+      })
+    })
+  }
 
-      <div className="user-profile-info">
-       <h2>Your Profile</h2>
-        <h3>User ID: {props.id}</h3>
-        <h3>Username: {props.username}</h3>
-        <h3>First Name: {props.firstname}</h3>
-        <h3>Last Name: {props.lastname}</h3>
-        <h3>E-mail: {props.email}</h3>
+  
+
+  render(){
+    this.getCards();
+    if(this.state.gotCards){
+      return (
+        <div className='user-profile'>
+          <Header />
+
+          <div className="user-profile-info">
+          <h2>Your Profile</h2>
+            <h3>User ID: {this.props.id}</h3>
+            <h3>Username: {this.props.username}</h3>
+            <h3>First Name: {this.props.firstname}</h3>
+            <h3>Last Name: {this.props.lastname}</h3>
+            <h3>E-mail: {this.props.email}</h3>
+            <h3>Number of cards: {this.state.cards.length}</h3>
+          </div>
+
+          <Footer />
+        </div>
+      )
+    }
+    return (
+      <div className='user-profile'>
+        <Header />
+
+        <div className="user-profile-info">
+          <h1>Loading Card Info</h1>
+        </div>
+
+        <Footer />
       </div>
-
-      <Footer />
-    </div>
     )
+    
+  }
+  
 }
 
 export default UserProfile;
