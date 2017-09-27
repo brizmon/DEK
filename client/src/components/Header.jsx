007d453ff3e87fab7e343a32093a1853d7b2ea3c
Briseida Montiel <brizism@gmail.com>
2017-09-27 14:51:56 -0400

    update Header component with Materialize classes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,24 +4,23 @@ import {Link} from 'react-router-dom';
 const Header = (props) => {
 
     return (
-      <div className='header'>
-
-        <h1 className='header-link-to-main'><Link to="/main">DEK</Link></h1>
-
-        <div className='nav'>
-
-          <div className='bars'>
-            <i className="fa fa-bars" aria-hidden="true"></i>
+      <div>
+        
+        <nav className="default" role="navigation">
+          <div className="nav-wrapper">
+            <Link to="/main"><h1 className="brand-logo left">Dek</h1></Link>
+            <a href="#!" data-activates="mobile-demo" className="button-collapse show-on-large"><i className="material-icons">menu</i></a>
+            <ul className="side-nav" id="mobile-demo">
+              <li><Link className='nav-link' Link to="/userprofile">My Profile</Link></li>
+              <li><Link className='nav-link' Link to="/aboutapp">About DEK</Link></li>
+              <li><Link className='nav-link' Link to="/">Log out</Link></li>
+              <li><Link className='nav-link' Link to="/main">Home</Link></li>
+              <li><Link className='nav-link' Link to="/createcard">Create Card</Link></li>
+              <li><Link className='nav-link' Link to="/editcards">Edit Cards</Link></li>
+              <li><Link className='nav-link' Link to="/pickquiztype">Pick Quiz</Link></li>
+            </ul>
           </div>
-
-          <div className="nav-dropdown">
-            <Link className='nav-link' Link to="/userprofile">My Profile</Link>
-            <Link className='nav-link' Link to="/aboutapp">About DEK</Link>
-            <Link className='nav-link' Link to="/">Log out</Link>
-          </div>
-
-        </div>
-
+        </nav>
       </div>
     )
   }
